b1078f4c92d2b832b15087cad33ec06d98e1e3b2
zeroban <theemptyspamfolder@gmail.com>
2024-07-07 20:02:48 -0400

    Answers to Week 4 assignment. Working with Middleweare REST methods and Postman

---
+++ 02-express-tutorial/authorize.js
@@ -0,0 +1,13 @@
+const authorize = (req, res, next) => {
+    console.log('authorize')
+    const { user } = req.query;
+    if (user === 'john') {
+        req.user = { name: 'john', id: 3 }
+        next()
+    }
+    else {
+        res.status(401).send('Unauthorized')
+    }
+}
+
+module.exports = authorize
